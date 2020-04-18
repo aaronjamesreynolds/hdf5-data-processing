964b08109163d2c1ca3397245016090a87bddb71
Aaron J. Reynolds <reynolaa@oregonstate.edu>
2020-04-18 14:26:44 -0400

    RMS - generate state names

--- calc-rms.py
+++ calc-rms.py
@@ -3,10 +3,20 @@ import numpy as np
 
 # Input parameters
 fileName = 'master-standard.h5'
-totalNumberStates = 71
+totalNumberStates = 72
 
 # Open file
 file = h5.File(fileName,'r')
+
+# Calculate RMS power at each state
+for x in range(0, totalNumberStates):
+  
+  # Formulate state name
+  if x < 10:    
+    stateName = 'STATE_000' + str(x)
+  else:
+    stateName = 'STATE_00' + str(x)
+  print(stateName)  
 
 # Close file
 file.close()
